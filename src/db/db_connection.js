4662218d73ac72d104fdb96a6638c3d17fdc150c
hafsa <hafsamdh18@gmail.com>
2022-06-18 21:52:24 +0100

    switched to atlas

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 
-//mongoose.connect(process.env.MONGODB_URL)
-mongoose.connect("mongodb://localhost:27017/ms-auth")
+mongoose.connect(process.env.MONGODB_URL)
+//mongoose.connect("mongodb://localhost:27017/ms-auth")
